5c39a96838216877627cf21221ed9eada1ce147d
fedin <ktec42lmicb>
2022-11-11 20:33:35 +0400

    Initial commit

---
+++ src/test/java/com/fprojects/articles_list/ArticlesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fprojects.articles_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
